dce5e7b568f45ccee3a7e5dc4a2e2398e803c738
paradizzo <80402261+paradizzo@users.noreply.github.com>
2021-04-19 13:05:13 -0300

    Add files via upload

---
+++ FAQ accordion card/script.js
@@ -0,0 +1,40 @@
+function HideAndShow() {
+    var x = document.getElementById('id1');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
+function HideAndShow2() {
+    var x = document.getElementById('id2');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
+function HideAndShow3() {
+    var x = document.getElementById('id3');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
+function HideAndShow4() {
+    var x = document.getElementById('id4');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
+function HideAndShow5() {
+    var x = document.getElementById('id5');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
